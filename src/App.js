277f7c4962740f3abd38b6462b0eebd6e300df0a
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2023-06-12 15:35:21 +0300

    project init

--- src/App.js
+++ src/App.js
@@ -7,13 +7,20 @@ import MapSample2 from './jsxSample/MapSample2'
 import MapSample3 from './jsxSample/MapSample3'
 import JsxEvents from './jsxSample/JsxEvents'
 import CssIntro from './cssSample/CssIntro'
+import Navbar from './template/Navbar'
+import Header from './template/Header'
+import PageContent from './template/PageContent'
+import SiteFooter from './template/SiteFooter'
 
 
 
 function App() {
   return (
     <>
-   <CssIntro></CssIntro>
+  <Navbar></Navbar>
+  <Header></Header>
+  <PageContent></PageContent>
+  <SiteFooter></SiteFooter>
     </>
   )
 }
